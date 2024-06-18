368f5207f74b2ab90458661a63390907101cb8bc
MrSquatch <86651695+MrSquatch@users.noreply.github.com>
2024-06-17 23:41:29 -0500

    Actualizacion: Se adapto flask_app para usar un archivo config externo

---
+++ Desarrollo/BF/Implementación/backend/api/config.py
@@ -0,0 +1,5 @@
+# Configuraciones específicas de tu entorno local
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'tu_usuario'
+MYSQL_PASSWORD = 'tu_contraseña'
+MYSQL_DB = 'bf_easy_bd'
--- Desarrollo/BF/Implementación/backend/api/flask_app.py
+++ Desarrollo/BF/Implementación/backend/api/flask_app.py
@@ -1,16 +1,18 @@
 from flask import Flask, jsonify
 from flask_mysqldb import MySQL
+import config
 
 app = Flask(__name__)
 
 # Configuración de la conexión a la base de datos
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'bf_easy_bd'
+app.config['MYSQL_HOST'] = config.MYSQL_HOST
+app.config['MYSQL_USER'] = config.MYSQL_USER
+app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
+app.config['MYSQL_DB'] = config.MYSQL_DB
 
 mysql = MySQL(app)
 
+# Configuracion de acceso a tablas
 table_access = ['alumnos','administradores','libros']
 
 def has_access(table_name):
@@ -20,7 +22,8 @@ def has_access(table_name):
     except Exception as e:
         print(f'{type(e)}: {e}')
         return False
-    
+
+# Rutas de la API
 @app.route('/api/')
 def specify_table_error():
     return jsonify({'error':'You must specify a table in \'api/\'!'})
@@ -41,5 +44,6 @@ def get_access_table_data(tabla):
     except Exception as e:
         return str(e)
 
+# Run app
 if __name__ == '__main__':
     app.run(debug=True)
